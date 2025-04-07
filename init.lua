eb534c54a769994b52a1f13621dd875568cf0534
Ben Ledingham <benledingham1@gmail.com>
2025-04-06 17:04:12 -0700

    init

---
+++ init.lua
@@ -0,0 +1,95 @@
+
+----------------------
+-- Misc --
+----------------------
+vim.cmd("set shiftwidth=2")
+vim.opt.guicursor = "n:block,i:ver25"
+
+----------------------
+-- Lazy Setup --
+----------------------
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons",
+	"MunifTanjim/nui.nvim",
+      },
+    }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+----------------------
+-- Telescope Config --
+----------------------
+
+local builtin = require("telescope.builtin")
+
+----------------------
+-- Theme --
+----------------------
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+----------------------
+-- Tree Sitter --
+----------------------
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "python"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+----------------------
+-- Web Dev Icons --
+----------------------
+require("nvim-web-devicons").setup()
+
+----------------------
+-- KeyMappings --
+----------------------
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle<CR>', {})
